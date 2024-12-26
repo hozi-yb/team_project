031e39a342e5fecaeb6b59a28196eff6cf372ebc
Dowill-hob <wiewk12365@gmail.com>
2024-12-26 17:14:23 +0900

    데이터 정리

--- data_array.py
+++ data_array.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import folium
-from folium.plugins import HeatMap
+from folium.plugins import HeatMap, marker_cluster
 
 # datas = pd.read_csv("./data/한국에너지기술연구원_신재생자원지도데이터_태양자원_천리안2호_수평면전일사량_20200831.csv", encoding="cp949")
 
@@ -9,25 +9,75 @@ from folium.plugins import HeatMap
 #     datas = datas.drop(columns=col)
 
 # datas.to_csv("./data/2020-08_1.csv",encoding="EUC-KR",index=False)
-# # 데이터 불러오기
+# data call
 file_path = './data/2020-08.csv'
 data = pd.read_csv(file_path, encoding='cp949')
+data.columns = ["위도","경도","2020-08"]
+filtered_data = data[(data['2020-08'] >= 2.89) & (data['2020-08'] <= 4.4)]
+# data.sort_values("2020-08")
+# data Normalization
+# data.to_csv("./data/2020-08.csv",encoding="EUC-KR",index=False)
+# 해안선 정리
+# idx_lon = data[(data["위도"]>=36.09235624359383) & (data["위도"]<=37.27578688222246)&(data["경도"]>=129.48570288602764)].index
+# print(idx_lon)
+
+for i in range(681529):
+    filtered_data = filtered_data[i][2] * 3.2 * 31
+    print(filtered_data)
+
+# for id in filtered_data.columns[2]:
+#     filtered_data= filtered_data.drop(index=id)
+
+# filtered_data.to_csv("./data/2020-08.csv",encoding="EUC-KR",index=False)
+
+
+
+# Cneter location
+# map_center =[filtered_data['위도'].mean(),filtered_data['경도'].mean()]
+# solar_map = folium.Map(location=map_center, zoom_start=7)
+
+# # created Heatmap data
+# heat_data = filtered_data[['위도', '경도', '2020-08']].values.tolist()
+# HeatMap(heat_data, radius=12).add_to(solar_map)
+
+# solar_map.save('solar_recommendations_with_click_popup.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # 필터링 기준 설정
-# min_irradiance = 500  # 최소 일사량 (사용자 지정 가능)
-
-data.columns = ["위도","경도","2020-08"]
+# min_irradiance = 3.0788717764918605 # 최소 일사량 (사용자 지정 가능)
+# max_irradiance = 4.0788717764918605
+# min_irradiance = [data['2020-08'].mean()]
 # 필터링
-# filtered_data = data[data['합계 일사량(MJ/m2)'] >= min_irradiance]
 
-# 해안선 정리
-idx_lon = data[(data["경도"]>=129.9) & (data["경도"]<133)].index
-print(idx_lon)
 
-# for id in idx_lon:
-#     data = data.drop(index=id)
-
-data.columns = ["위도","경도","2020-08"]
 # idx_lat = data[(data["위도"]<= 38.462)].index
 # print(idx_lat)
 # data.sort_values("경도")
@@ -36,12 +86,3 @@ data.columns = ["위도","경도","2020-08"]
 
 # # 단위도 바꿔야 합니다.
 # # 1kwh/m^2 단위 day 이므로 MJ/m^2 단위 month는 (kwh/m^2 x 3.6MJ/m^2 x 31day) 입니다.
-
-
-map_center = [data['위도'].mean(),data['경도'].mean()]
-solar_map = folium.Map(location=map_center, zoom_start=7)
-
-# 히트맵 데이터 생성
-heat_data = data[['위도', '경도', '2020-08']].values.tolist()
-HeatMap(heat_data, radius=15).add_to(solar_map)
-solar_map.save('solar_recommendations_with_click_popup.html')
